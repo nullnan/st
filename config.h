66cab52c2d3601bfc8f79772001d35a717acea43
Null NaN <nullnan0x0000@gmail.com>
2020-12-24 23:56:23 +0800

    add blinkcursor-patch

--- config.h
+++ config.h
@@ -142,13 +142,19 @@ static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
- * Default shape of cursor
- * 2: Block ("█")
- * 4: Underline ("_")
- * 6: Bar ("|")
- * 7: Snowman ("☃")
+ * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lpare-s-rparen:CSI-Ps-SP-q.1D81
+ * Default style of cursor
+ * 0: Blinking block
+ * 1: Blinking block (default)
+ * 2: Steady block ("█")
+ * 3: Blinking underline
+ * 4: Steady underline ("_")
+ * 5: Blinking bar
+ * 6: Steady bar ("|")
+ * 7: Blinking st cursor
+ * 8: Steady st cursor
  */
-static unsigned int cursorshape = 6;
+static unsigned int cursorshape = 5;
 
 /*
  * Default columns and rows numbers
